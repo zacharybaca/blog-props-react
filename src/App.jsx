b8319662e6968c44023166515a3eb4312afeb377
zacharybaca <zachary.baca@msn.com>
2024-06-17 11:27:51 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import data from "../blog-data.json";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div id="App">
+        <h1>Hello!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
